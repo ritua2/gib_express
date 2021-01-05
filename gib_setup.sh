6bda774d562f462273b85c4d647fb2cce2f1818c
Saumya Shah <saumyashahori18@gmail.com>
2021-01-05 07:40:27 -0800

    Update gib_setup.sh

--- gib_setup.sh
+++ gib_setup.sh
@@ -7,7 +7,7 @@ docker-compose up -d
 docker exec -itd manager_node /bin/bash -c "ssh-keygen -t rsa -N newpass  -f rsync_wetty.key; gunicorn -w \$gthreads -b 0.0.0.0:5000 traffic:app \&; exit;"
 docker exec -itd greyfish /bin/bash -c "cd \/grey; \/grey\/API_Daemon.sh \-up;sleep 10;"
 cd ../springipt
-apt install maven
+apt install maven -y
 mvn clean package
 docker-compose up -d --build
 docker exec -it tomcat_springipt /bin/bash -c "rm \-rf keystore; sleep 5; \$JAVA_HOME\/bin\/keytool \-genkey \-alias tomcat \-keyalg RSA \-keystore keystore; echo \"out of springipt after generating key \"; exit"
